1a38cb117bc91d3040fe2fe44914091d5d40d790
Jose A. Abell <jaabell@uandes.cl>
2023-08-31 14:06:50 -0400

    Files for example6.py

--- gmsh2opensees/g2o_elements_functions.py
+++ gmsh2opensees/g2o_elements_functions.py
@@ -84,14 +84,26 @@ def get_elements_and_nodes_in_physical_group(groupname, gmshmodel):
 	for e in entities:
 		elementTypes, elementTags, nodeags = gmshmodel.mesh.getElements(dim, e)
 
+		if len(elementTypes) == 0:
+			print(f"get_elements_and_nodes_in_physical_group({groupname=}, {gmshmodel=})  ")
+			print("Physical group has no elements! (Mesh empty, try meshing beforehand.) ")
+			print(f"{dim=} ")
+			print(f"{e=} ")
+			print(f"{tag=} ")
+			exit(-1)
+
 		if len(elementTypes) != 1:
+			print(f"get_elements_and_nodes_in_physical_group({groupname=}, {gmshmodel=})  ")
 			print("Cannot handle more than one element type at this moment. Contributions welcome. ")
+			print(f"{elementTypes=} ")
 			exit(-1)
 
 		if base_element_type == -1:
 			base_element_type = elementTypes[0]
 		elif elementTypes[0] != base_element_type:
+			print(f"get_elements_and_nodes_in_physical_group({groupname=}, {gmshmodel=})  ")
 			print("All entities of physical group should have the same element type. Contributions welcome. ")
+			print(f"{elementTypes=} ")
 			exit(-1)
 
 
--- gmsh2opensees/g2o_viz.py
+++ gmsh2opensees/g2o_viz.py
@@ -7,9 +7,9 @@
 import os
 
 if os.name == 'nt':
-	import openseespy.opensees as ops
+    import openseespy.opensees as ops
 else:   #not checked in mac
-	import opensees as ops
+    import opensees as ops
 
 
 
@@ -19,156 +19,219 @@ from gmsh2opensees.g2o_nodes_functions import get_all_nodes, get_displacements_a
 
 
 def visualize_displacements_in_gmsh(gmshmodel, nodeTags=[], viewnum=-1,step=0,time=0.,new_view_name="Displacements", component=-1):
-	"""
-	Visualize displacement field in gmsh, only for defined nodes.
-	If view-number is not supplied, will create a new view. 
-	If you want an animation, create the view outside (or call this function without specifying a view),
-	this function will output the view number (handle), then you can call this with
-	fata for subsequent steps by specifying a different step and time, but passing
-	the view number. 
-	
-	Call once per time-step.
+    """
+    Visualize displacement field in gmsh, only for defined nodes.
+    If view-number is not supplied, will create a new view. 
+    If you want an animation, create the view outside (or call this function without specifying a view),
+    this function will output the view number (handle), then you can call this with
+    fata for subsequent steps by specifying a different step and time, but passing
+    the view number. 
+    
+    Call once per time-step.
 
-	You can also change the default name of the view 
-	"""
-	import gmsh
+    You can also change the default name of the view 
+    """
+    import gmsh
 
-	if len(nodeTags) == 0:
-		allGmshNodeTags, _ = get_all_nodes(gmshmodel)	
-	else:
-		allGmshNodeTags = nodeTags
-	displacement_data = get_displacements_at_nodes(allGmshNodeTags, component)
+    if len(nodeTags) == 0:
+        allGmshNodeTags, _ = get_all_nodes(gmshmodel)    
+    else:
+        allGmshNodeTags = nodeTags
+    displacement_data = get_displacements_at_nodes(allGmshNodeTags, component)
 
-	if viewnum==-1:
-		viewnum = gmsh.view.add(new_view_name)
+    if viewnum==-1:
+        viewnum = gmsh.view.add(new_view_name)
 
-	gmsh.view.addHomogeneousModelData(
-		tag=viewnum, 
-		step=step,
-		time=time, 
-		modelName=gmsh.model.getCurrent(),
-		dataType="NodeData",
-		numComponents=-1,
-		tags=allGmshNodeTags,
-		data=displacement_data.reshape((-1))
-	)
+    gmsh.view.addHomogeneousModelData(
+        tag=viewnum, 
+        step=step,
+        time=time, 
+        modelName=gmsh.model.getCurrent(),
+        dataType="NodeData",
+        numComponents=-1,
+        tags=allGmshNodeTags,
+        data=displacement_data.reshape((-1))
+    )
 
-	return viewnum
+    return viewnum
 
 
 
 def visualize_eigenmode_in_gmsh(gmshmodel, mode=1, f=0, viewnum=-1,step=0,time=0.,new_view_name="Mode",animate=False,normalize=True, nsteps=10, factor=0.):
-	"""
-	Visualize eigenvector displacement field in gmsh, only for defined nodes.
-	If view-number is not supplied, will create a new view. 
-	If you want an animation, create the view outside (or call this function without specifying a view),
-	this function will output the view number (handle), then you can call this with
-	fata for subsequent steps by specifying a different step and time, but passing
-	the view number. 
-	
-	Call once per time-step.
+    """
+    Visualize eigenvector displacement field in gmsh, only for defined nodes.
+    If view-number is not supplied, will create a new view. 
+    If you want an animation, create the view outside (or call this function without specifying a view),
+    this function will output the view number (handle), then you can call this with
+    fata for subsequent steps by specifying a different step and time, but passing
+    the view number. 
+    
+    Call once per time-step.
 
-	You can also change the default name of the view 
-	"""
-	import gmsh
+    You can also change the default name of the view 
+    """
+    import gmsh
 
-	allGmshNodeTags, coords = get_all_nodes(gmshmodel)
-	eigenvector_data = get_eigenvector_at_nodes(allGmshNodeTags, mode)
+    allGmshNodeTags, coords = get_all_nodes(gmshmodel)
+    eigenvector_data = get_eigenvector_at_nodes(allGmshNodeTags, mode)
 
-	if viewnum==-1:
-		viewnum = gmsh.view.add(new_view_name + f" {mode} {f=}")
+    if viewnum==-1:
+        viewnum = gmsh.view.add(new_view_name + f" {mode} {f=}")
 
-	if not animate:
+    if not animate:
 
-		gmsh.view.addHomogeneousModelData(
-			tag=viewnum, 
-			step=step,
-			time=time, 
-			modelName=gmsh.model.getCurrent(),
-			dataType="NodeData",
-			numComponents=-1,
-			tags=allGmshNodeTags,
-			data=eigenvector_data.reshape((-1))
-		)
+        gmsh.view.addHomogeneousModelData(
+            tag=viewnum, 
+            step=step,
+            time=time, 
+            modelName=gmsh.model.getCurrent(),
+            dataType="NodeData",
+            numComponents=-1,
+            tags=allGmshNodeTags,
+            data=eigenvector_data.reshape((-1))
+        )
 
-	else:
+    else:
 
-		for step in range(nsteps):
-			gmsh.view.addHomogeneousModelData(
-				tag=viewnum, 
-				step=step,
-				time=float(time), 
-				modelName=gmsh.model.getCurrent(),
-				dataType="NodeData",
-				numComponents=-1,
-				tags=allGmshNodeTags,
-				data=(eigenvector_data*cos(step/nsteps*2*pi)).reshape((-1))
-			)
+        for step in range(nsteps):
+            gmsh.view.addHomogeneousModelData(
+                tag=viewnum, 
+                step=step,
+                time=float(time), 
+                modelName=gmsh.model.getCurrent(),
+                dataType="NodeData",
+                numComponents=-1,
+                tags=allGmshNodeTags,
+                data=(eigenvector_data*cos(step/nsteps*2*pi)).reshape((-1))
+            )
 
-	gmsh.view.option.setNumber(viewnum, "VectorType", 5)
+    gmsh.view.option.setNumber(viewnum, "VectorType", 5)
 
 
-	if factor == 0:
-		dx = coords[:,0].max() - coords[:,0].min()
-		dy = coords[:,1].max() - coords[:,1].min()
-		dz = coords[:,2].max() - coords[:,2].min()
-		eigenmax = abs(eigenvector_data).max()
-		factor = 0.1 * sqrt(dx**2 + dy**2 + dz**2) / eigenmax
+    if factor == 0:
+        dx = coords[:,0].max() - coords[:,0].min()
+        dy = coords[:,1].max() - coords[:,1].min()
+        dz = coords[:,2].max() - coords[:,2].min()
+        eigenmax = abs(eigenvector_data).max()
+        factor = 0.1 * sqrt(dx**2 + dy**2 + dz**2) / eigenmax
 
-	gmsh.view.option.setNumber(viewnum, "DisplacementFactor", factor)
+    gmsh.view.option.setNumber(viewnum, "DisplacementFactor", factor)
 
-	return viewnum
+    return viewnum
 
 
 def visualize_eleResponse_in_gmsh(gmshmodel, eleTags, args, viewnums=[],step=0,time=0.,new_view_name=f"eleResponse"):
-	"""
-	Visualize a per-element field in gmsh, only for defined elements.
-	If the eleResponse is a vector, will add as many views as vector
-	components.
+    """
+    Visualize a per-element field in gmsh, only for defined elements.
+    If the eleResponse is a vector, will add as many views as vector
+    components.
 
-	If view-number is not supplied, will create a new view. 
-	If you want an animation, create the view outside (or call this function without specifying a view),
-	this function will output the view number (handle), then you can call this with
-	fata for subsequent steps by specifying a different step and time, but passing
-	the view number. 
-	
-	Call once per time-step.
+    If view-number is not supplied, will create a new view. 
+    If you want an animation, create the view outside (or call this function without specifying a view),
+    this function will output the view number (handle), then you can call this with
+    fata for subsequent steps by specifying a different step and time, but passing
+    the view number. 
+    
+    Call once per time-step.
 
-	You can also change the default name of the view 
-	"""
-	import gmsh
+    You can also change the default name of the view 
+    """
+    import gmsh
 
 
-	one_eleResponse_data = ops.eleResponse(eleTags[0], args)
+    one_eleResponse_data = ops.eleResponse(eleTags[0], args)
 
-	print(f"eleResponse({args=}) for {eleTags[0]=} = {one_eleResponse_data}")
+    print(f"eleResponse({args=}) for {eleTags[0]=} = {one_eleResponse_data}")
 
-	Ncomponents = len(one_eleResponse_data)
-	Nelements = len(eleTags)
+    Ncomponents = len(one_eleResponse_data)
+    Nelements = len(eleTags)
 
-	eleResponse_data = zeros((Nelements, Ncomponents))
+    eleResponse_data = zeros((Nelements, Ncomponents))
 
-	for i, eleTag in enumerate(eleTags):
-		eleResponse_data[i,:] = ops.eleResponse(eleTag, args)
+    for i, eleTag in enumerate(eleTags):
+        eleResponse_data[i,:] = ops.eleResponse(eleTag, args)
 
-	if len(viewnums)==0:
-		for i in range(Ncomponents):
-			viewnums.append(gmsh.view.add(new_view_name + f" {i}"))
+    if len(viewnums)==0:
+        for i in range(Ncomponents):
+            viewnums.append(gmsh.view.add(new_view_name + f" {i}"))
 
-	print(f"{eleTags=}")
-	print(f"{eleResponse_data=}")
+    #print(f"{eleTags=}")
+    #print(f"{eleResponse_data=}")
 
-	for i in range(Ncomponents):
-		gmsh.view.addHomogeneousModelData(
-			tag=viewnums[i], 
-			step=step,
-			time=time, 
-			modelName=gmsh.model.getCurrent(),
-			dataType="ElementData",
-			numComponents=-1,
-			tags=eleTags,
-			data=eleResponse_data[:,i].reshape((-1))
-		)
+    for i in range(Ncomponents):
+        gmsh.view.addHomogeneousModelData(
+            tag=viewnums[i], 
+            step=step,
+            time=time, 
+            modelName=gmsh.model.getCurrent(),
+            dataType="ElementData",
+            numComponents=-1,
+            tags=eleTags,
+            data=eleResponse_data[:,i].reshape((-1))
+        )
 
-	return viewnums
+    return viewnums
+
+def visualize_eleNodeResponse_in_gmsh(gmshmodel, eleTags, args, viewnums=[],step=0,time=0.,new_view_name=f"eleResponse"):
+    """
+    Visualize a per-element field in gmsh, only for defined elements.
+    If the eleResponse is a vector, will add as many views as vector
+    components.
+
+    If view-number is not supplied, will create a new view. 
+    If you want an animation, create the view outside (or call this function without specifying a view),
+    this function will output the view number (handle), then you can call this with
+    fata for subsequent steps by specifying a different step and time, but passing
+    the view number. 
+    
+    Call once per time-step.
+
+    You can also change the default name of the view 
+    """
+    import gmsh
+
+
+    one_eleResponse_data = ops.eleResponse(eleTags[0], args)
+    one_eleNodes_data = ops.eleNodes(eleTags[0], args)
+    numnodes = len(one_eleNodes_data)
+
+    print(f"eleResponse({args=}) for {eleTags[0]=} {one_eleResponse_data=} {one_eleNodes_data=}")
+
+    Ncomponents = len(one_eleResponse_data)//numnodes
+    Nelements = len(eleTags)
+
+    print(f"{numnodes=}")
+    print(f"{Ncomponents=}")
+    print(f"{Nelements=}")
+
+    eleResponse_data = zeros((Nelements, len(one_eleResponse_data)))
+
+    for i, eleTag in enumerate(eleTags):
+        eleResponse_data[i,:] = ops.eleResponse(eleTag, args)
+
+    if len(viewnums)==0:
+        for i in range(Ncomponents):
+            viewnums.append(gmsh.view.add(new_view_name + f" {i}"))
+
+    #print(f"{eleTags=}")
+    #print(f"{eleResponse_data=}")
+
+    for i in range(Ncomponents):
+        # thisdata=eleResponse_data[:,i::Ncomponents]
+        thisdata=eleResponse_data[:,i::Ncomponents]
+        # aux = thisdata[:,9]
+        # thisdata[:,9] = thisdata[:,8]
+        # thisdata[:,8] = aux
+        gmsh.view.addHomogeneousModelData(
+            tag=viewnums[i], 
+            step=step,
+            time=time, 
+            modelName=gmsh.model.getCurrent(),
+            dataType="ElementNodeData",
+            numComponents=1,
+            tags=eleTags,
+            data=thisdata.reshape((-1))
+        )
+
+    return viewnums
 
